893fdacebdea44975f649a94826a133addbca043
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-04-25 22:44:57 +0800

    Assignment 4 Request 1 change text done

--- Week-2/Assignment-4/action.js
+++ Week-2/Assignment-4/action.js
@@ -1,9 +1,6 @@
-const banner = document.getElementById('banner');
+
 const headline = document.getElementById('headline');
 
-banner.addEventListener('click', () => {
-//     document.querySelector('headline').innerHTML = `
-//   <h1>Have a Good Time!</h1>`;
-document.getElementById("headline").value = Have a Good Time!
-
-}
+headline.addEventListener("click", () => {
+  headline.textContent = "Have a Good Time!";
+});
