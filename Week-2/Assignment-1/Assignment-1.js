123e90a0e0bffc54077d26a3a91c18d76049230c
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-04-25 15:36:55 +0800

    assignment 1 done

--- Week-2/Assignment-1/Assignment-1.js
+++ Week-2/Assignment-1/Assignment-1.js
@@ -13,9 +13,18 @@ function max(numbers) {
 
     function findPosition(numbers, target) {
     // your code here, for-loop method preferred
-    }
+    for ( i = 0 ; i < numbers.length ; i += 1) {
+        if ( numbers[i]== target ){
+            return i
+            break;
+        }
+    }  
+    return -1
+}
+
     console.log( max([1, 2, 4, 5]) ); // should print 5
     console.log( max([5, 2, 7, 1, 6]) ); // should print 7
     console.log( findPosition([5, 2, 7, 1, 6], 5) ); // should print 0
     console.log( findPosition([5, 2, 7, 1, 6], 7) ); // should print 2
-    console.log( findPosition([5, 2, 7, 7, 7, 1, 6], 7) ); // should print 2 (the first position) console.log( findPosition([5, 2, 7, 1, 6], 8) ); // should print -1
+    console.log( findPosition([5, 2, 7, 7, 7, 1, 6], 7) ); // should print 2 (the first position) 
+    console.log( findPosition([5, 2, 7, 1, 6], 8) ); // should print -1
