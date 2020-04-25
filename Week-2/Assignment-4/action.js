90736f6ed6093e8d99d8d32d5ce3594bac74cff5
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-04-25 23:41:22 +0800

    Create Request 2 Fixed Menu

--- Week-2/Assignment-4/action.js
+++ Week-2/Assignment-4/action.js
@@ -1,6 +1,11 @@
 
 const headline = document.getElementById('headline');
+const menu = document.getElementById('menu');
 
 headline.addEventListener("click", () => {
   headline.textContent = "Have a Good Time!";
 });
+
+headline.addEventListener("click", () => {
+   
+  });
