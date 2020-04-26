e7833b38053dfd0337f6f5cae563cb5e1af16e82
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-04-26 22:11:53 +0800

    Fix Assignment 3

--- Week-2/Assignment-3/Assignment-3.js
+++ Week-2/Assignment-3/Assignment-3.js
@@ -1,6 +1,12 @@
 function avg(data) {
     // your code here
-    return (data.products[0].price + data.products[1].price + data.products[2].price) / data.size
+    var total = 0;
+    for ( i = 0 ; i < data.size ; i +=1 ){
+       total += data.products[i].price
+        }
+
+    return total / data.size
+    // return (data.products[0].price + data.products[1].price + data.products[2].price) / data.size
     }
 
     console.log( 
