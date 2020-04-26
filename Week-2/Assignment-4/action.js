d46c3cdf61d1c7ede88e786c8ce6d4c485fe54f2
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-04-26 18:45:34 +0800

    Request 3: Click to Show More Content Boxes Done

--- Week-2/Assignment-4/action.js
+++ Week-2/Assignment-4/action.js
@@ -3,6 +3,8 @@ const welcome = document.querySelector(".welcome");
 const menu = document.querySelector(".menu");
 const icon = document.querySelector(".icon");
 const fixed = document.querySelector(".fixed");
+const action = document.querySelector(".action");
+const box2 = document.querySelector(".box2");
 
 welcome.addEventListener("click", () => {
     welcome.textContent = "Have a Good Time!";
@@ -10,10 +12,14 @@ welcome.addEventListener("click", () => {
 
 menu.addEventListener("click", () => {   
     fixed.style.display = 'block';
-  });
+});
 
 icon.addEventListener("click", () => {   
     fixed.style.display = 'none';
-  });
+});
+
+action.addEventListener("click", () => {   
+    box2.style.display = 'block';
+});
 
 
