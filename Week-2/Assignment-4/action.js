c5a6704631ac1656210974b720af4f837027bfab
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-04-26 18:29:15 +0800

    Request 2: Click to Show/Close Menu Done

--- Week-2/Assignment-4/action.js
+++ Week-2/Assignment-4/action.js
@@ -1,18 +1,19 @@
 
 const welcome = document.querySelector(".welcome");
 const menu = document.querySelector(".menu");
+const icon = document.querySelector(".icon");
 const fixed = document.querySelector(".fixed");
 
 welcome.addEventListener("click", () => {
     welcome.textContent = "Have a Good Time!";
 });
 
-menu.addEventListener("click", () => {
-    if (fixed.style.display == 'none') {
-        fixed.style.display = 'block';
-      } else {                    
-        fixed.style.display = 'none';
-      }        
+menu.addEventListener("click", () => {   
+    fixed.style.display = 'block';
+  });
+
+icon.addEventListener("click", () => {   
+    fixed.style.display = 'none';
   });
 
 
