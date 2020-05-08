4955763c7cbd399c31d3398b7ea39373fb6d77f7
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-05-08 20:15:41 +0800

    import json

--- Week-4/Assignment-2/index.js
+++ Week-4/Assignment-2/index.js
@@ -1,44 +1,21 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-if (xhr.readyState === 4) {
-    document.getElementById('list').innerHTML = xhr.responseText;
-    var device = JSON.parse(xhr.responseText);
-    console.log(device);
-    }
-};
-    
-// function ajax(src, callback){
-    xhr.open('GET', "https://cwpeng.github.io/live-records-samples/data/products.json");
-// }
-// function render(data){
-//     document.createElement() and appendChild() methods are preferred.
-//     xhr.render();
-// }
-
-// ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
-//     render(response);
-// }); 
-	 xhr.send(); 	
-
-
-//===================================================================================
-
-// const express = require('express');
-// const app = express();
-
-// function ajax(src, callback){
-//     app.get (src, (req, res) => {
-//         request(
-//             url: src ,
-//             method: 'GET',
-//          );
-//     });
-//         console.log(body);
-//         response.end(JSON.stringify(body));   
-// };
-// function render(data){
+function ajax(src, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open( 'GET', 'src' , false);
+    // var data = xhr.responseText;
+    xhr.send();
+    console.log(src);
    
-// }
-// ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
-//     render(response);
-//     }); 
+};
+
+// function render(data){
+//     list.innerHTML="";
+//     let device;
+// 		for(let i=0;i<data.length;i++){
+// 			device = data[i];
+// 			list.innerHTML += device.name + "<br/>" + device.price + "<br/>" + device.description + "<br/>";
+// 		}
+// };
+
+ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
+     render(response);
+}); 
