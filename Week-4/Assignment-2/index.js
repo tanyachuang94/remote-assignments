536b6a3529864894c8254f5a06f5f1e6c882eb42
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-05-08 22:01:41 +0800

    Fix call ajax twice bug

--- Week-4/Assignment-2/index.js
+++ Week-4/Assignment-2/index.js
@@ -1,22 +1,24 @@
 function ajax(src, callback){
-    var url = src;
+  
     var xhr = new XMLHttpRequest();
-    xhr.open( 'GET', url , false);
-    // var data = JSON.parse(xhr.responseText);
+    xhr.open( 'GET', src , false);
+    xhr.onload = function(){
+        callback(this.responseText);
+    }; 
     xhr.send();
-    console.log(xhr.responseText);
-    // callback();
 };
 
-// function render(data){
+function render(data){
+    var data = JSON.parse(data);
+    console.log(data);
     // let list = document.getElementById("list");
-//     list.innerHTML="";
-//     let device;
-// 		for(let i=0;i<data.length;i++){
-// 			device = data[i];
-// 			list.innerHTML += device.name + "<br/>" + device.price + "<br/>" + device.description + "<br/>";
-// 		}
-// };
+    // list.innerHTML="";
+    // let device;
+	// 	for(let i=0;i<data.length;i++){
+	// 		device = data[i];
+	// 		list.innerHTML += device.name + "<br/>" + device.price + "<br/>" + device.description + "<br/>";
+	// 	}
+};
 
 ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
      render(response);
