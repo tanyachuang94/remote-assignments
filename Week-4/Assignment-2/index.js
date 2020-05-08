c114fd480f1dad95528e8f00f9c59d7d76a13542
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-05-08 12:24:34 +0800

    Assignment 2 get JSON

--- Week-4/Assignment-2/index.js
+++ Week-4/Assignment-2/index.js
@@ -1,20 +1,44 @@
-const express = require('express');
-const app = express();
-
-function ajax(src, callback){
-    app.get (src, callback){
-        // request(
-        //     url: ,
-        //     method: 'GET',
-        //  );
-        console.log(body);
-        response.end(JSON.stringify(body));
-        }
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+if (xhr.readyState === 4) {
+    document.getElementById('list').innerHTML = xhr.responseText;
+    var device = JSON.parse(xhr.responseText);
+    console.log(device);
+    }
 };
-// function render(data){
-    // your code here.
-    // document.createElement() and appendChild() methods are preferred.
+    
+// function ajax(src, callback){
+    xhr.open('GET', "https://cwpeng.github.io/live-records-samples/data/products.json");
 // }
-ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
-    render(response);
-    }); // you should get product information in JSON format and render data in the page
+// function render(data){
+//     document.createElement() and appendChild() methods are preferred.
+//     xhr.render();
+// }
+
+// ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
+//     render(response);
+// }); 
+	 xhr.send(); 	
+
+
+//===================================================================================
+
+// const express = require('express');
+// const app = express();
+
+// function ajax(src, callback){
+//     app.get (src, (req, res) => {
+//         request(
+//             url: src ,
+//             method: 'GET',
+//          );
+//     });
+//         console.log(body);
+//         response.end(JSON.stringify(body));   
+// };
+// function render(data){
+   
+// }
+// ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
+//     render(response);
+//     }); 
