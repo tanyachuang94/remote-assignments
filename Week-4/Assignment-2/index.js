52aac1183dc862a8795d4854bc9f765cb71cc1bd
tanyachuang94 <50539276+tanyachuang94@users.noreply.github.com>
2020-05-08 23:31:09 +0800

    Assignment 2 Done

--- Week-4/Assignment-2/index.js
+++ Week-4/Assignment-2/index.js
@@ -1,23 +1,21 @@
 function ajax(src, callback){
-  
     var xhr = new XMLHttpRequest();
-    xhr.open( 'GET', src , false);
+    xhr.open( 'GET', src , true);
     xhr.onload = function(){
         callback(this.responseText);
-    }; 
+        }; 
     xhr.send();
 };
 
 function render(data){
     var data = JSON.parse(data);
-    console.log(data);
-    // let list = document.getElementById("list");
-    // list.innerHTML="";
-    // let device;
-	// 	for(let i=0;i<data.length;i++){
-	// 		device = data[i];
-	// 		list.innerHTML += device.name + "<br/>" + device.price + "<br/>" + device.description + "<br/>";
-	// 	}
+    let list = document.getElementById("list");
+    list.innerHTML="";
+    let device;
+		for(let i=0;i<data.length;i++){
+			device = data[i];
+			list.innerHTML += "Name:" + device.name + "<br/> Price:" + device.price + "<br/> Description:" + device.description + "<br/>";
+		}
 };
 
 ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
